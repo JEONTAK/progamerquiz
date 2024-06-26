f789582f5c6e439716ca99e5c05636e1a0c9fc87
JEONTAK <tak980418@naver.com>
2024-06-26 16:50:02 +0900

    Initial commit

---
+++ src/test/java/com/pq/progamerquiz/ProgamerquizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pq.progamerquiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgamerquizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
