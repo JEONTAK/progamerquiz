244f9e5af54f45ff7ce747ccd340357617df8eab
JEONTAK <tak980418@naver.com>
2024-08-15 17:25:38 +0900

    2024/08/15 Update

--- src/main/java/com/pq/progamerquiz/domain/mainPage/MainController.java
+++ src/main/java/com/pq/progamerquiz/domain/mainPage/MainController.java
@@ -6,25 +6,29 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.Arrays;
 import java.util.List;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MainController {
 
-    @GetMapping("/quizzes")
+    @GetMapping("/")
     public String getQuizzes(Model model) {
-        List<String> menuItems = Arrays.asList("Who Are You?", "Bingo!", "Tic Tac Toe", "Piece of Puzzle", "What is Team?", "I Got You!");
+        List<String> menuItems = Arrays.asList("Who Are You?", "Bingo!", "Tic Tac Toe", "Piece of Puzzle",
+                "What is Team?", "I Got You!");
         model.addAttribute("menuItems", menuItems);
 
         List<Quiz> quizzes = Arrays.asList(
-                new Quiz("whoareyou.html", "/media/content/quizImg/quiz1.jpg", "Who Are You?", "Guess the Progamer."),
-                new Quiz("bingo.html", "/media/content/quizImg/quiz2.jpg", "Bingo!", "Make Bingo"),
-                new Quiz("tictactoe.html", "/media/content/quizImg/quiz3.jpg", "Tic Tac Toe", "Play Tic Tac Toe"),
-                new Quiz("pieceofpuzzle.html", "/media/content/quizImg/quiz4.jpg", "Piece of Puzzle", "Find Progamer on particular team"),
-                new Quiz("whatisteam.html", "/media/content/quizImg/quiz5.jpg", "What is Team?", "Find Team Using Hint"),
-                new Quiz("igotyou.html", "/media/content/quizImg/quiz6.jpg", "I Got You!", "Find Progamer Using Team")
+                new Quiz("quiz1", "/media/content/quizImg/quiz1.jpg", "Who Are You?", "Guess the Progamer."),
+                new Quiz("quiz2", "/media/content/quizImg/quiz2.jpg", "Bingo!", "Make Bingo"),
+                new Quiz("quiz3", "/media/content/quizImg/quiz3.jpg", "Tic Tac Toe", "Play Tic Tac Toe"),
+                new Quiz("quiz4", "/media/content/quizImg/quiz4.jpg", "Piece of Puzzle",
+                        "Find Progamer on particular team"),
+                new Quiz("quiz5", "/media/content/quizImg/quiz5.jpg", "What is Team?",
+                        "Find Team Using Hint"),
+                new Quiz("quiz6", "/media/content/quizImg/quiz6.jpg", "I Got You!", "Find Progamer Using Team")
         );
         model.addAttribute("quizzes", quizzes);
 
-        return "quizTemplate"; // 위의 Thymeleaf 템플릿이 quizTemplate.html이라고 가정합니다.
+        return "basic/main";
     }
 }
