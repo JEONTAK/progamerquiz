190409fd1685398d3a37e15fe6a437890f96020c
jeontak <tak980418@naver.com>
2024-09-27 14:06:43 +0900

    2024/09/27 : Update Coming Soon

--- src/main/resources/static/js/home.js
+++ src/main/resources/static/js/home.js
@@ -4,3 +4,19 @@ function toggleMenu() {
 }
 
 localStorage.removeItem('guideShown');
+
+// 퀴즈 클릭 핸들러 함수
+function handleQuizClick(hasUrl, url) {
+    if (hasUrl) {
+        // URL이 유효한 경우 해당 페이지로 이동
+        window.location.href = url;
+    } else {
+        // URL이 없을 경우 Coming Soon 오버레이 표시
+        document.getElementById('coming-soon-overlay').style.display = 'flex';
+    }
+}
+
+// 오버레이 닫기 함수
+function closeOverlay() {
+    document.getElementById('coming-soon-overlay').style.display = 'none';
+}
