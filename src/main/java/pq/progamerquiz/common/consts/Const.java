54cb4b29b696f86ce6e703103a3ef9d1c72a819b
jeontak <tak980418@naver.com>
2025-05-09 15:47:23 +0900

    refactor : 유저 제거 및 회원가입, 로그인 제거

--- src/main/java/pq/progamerquiz/common/consts/Const.java
+++
@@ -1,7 +0,0 @@
-package pq.progamerquiz.common.consts;
-
-public interface Const {
-
-    String PASSWORD_PATTERN = "^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,}$";
-
-}
--- src/main/java/pq/progamerquiz/controller/HomeController.java
+++ src/main/java/pq/progamerquiz/controller/HomeController.java
@@ -2,53 +2,81 @@ package pq.progamerquiz.controller;
 
 
 import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
-import org.springframework.web.bind.annotation.GetMapping;
-import pq.progamerquiz.domain.quiz.entity.Quiz;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import pq.progamerquiz.domain.quiz.dto.response.QuizResponse;
+import pq.progamerquiz.domain.quiz.entity.Quiz;
 import pq.progamerquiz.domain.quiz.service.QuizService;
 
 import java.util.List;
 
 @Controller
 @Slf4j
+@RequiredArgsConstructor
 public class HomeController {
 
     private final ResourceLoader resourceLoader;
     private final EntityManager em;
     private final QuizService quizService;
 
-    public HomeController(ResourceLoader resourceLoader, EntityManager em, QuizService quizService) {
-        this.resourceLoader = resourceLoader;
-        this.em = em;
-        this.quizService = quizService;
-    }
-
     @GetMapping("/")
     public String home(Model model) {
         log.info("Home Controller");
-        List<Quiz> quizzes  = em.createQuery("select q from Quiz q", Quiz.class).getResultList();
+        List<Quiz> quizzes  = quizService.findAll();
 
-        for (Quiz quiz : quizzes) {
+        quizzes.forEach(quiz -> {
             Resource image = resourceLoader.getResource("classpath:/static/images/quiz/" + quiz.getImageUrl());
-            String imageUrl;
-            if (image.exists() && image.isReadable()) {
-                imageUrl = "/images/quiz/" + quiz.getImageUrl();
-                quiz.setImageUrl(imageUrl);
-            } else {
-                // 이미지가 없으면 none.png 반환
-                imageUrl = "/images/none.png";
-                quiz.setImageUrl(imageUrl);
-            }
-
+            // 이미지가 없으면 none.png 반환
+            String imageUrl = image.exists() && image.isReadable() ? "/images/quiz/" + quiz.getImageUrl() : "/images/none.png";
+            quiz.setImageUrl(imageUrl);
             quizService.saveQuiz(quiz);
-        }
-        model.addAttribute("quizzes", quizzes);
+        });
+
+        List<QuizResponse> responses = quizzes.stream().map(quiz -> {
+            return QuizResponse.of(
+                    quiz.getId(),
+                    quiz.getUrl(),
+                    quiz.getImageUrl(),
+                    quiz.getAltText(),
+                    quiz.getTitle(),
+                    quiz.getDescription()
+            );
+        }).toList();
+
+        model.addAttribute("quizzes", responses);
         return "home";
     }
+
+    /*@GetMapping("/")
+    public ResponseEntity<List<QuizResponse>> home(Model model) {
+        log.info("Home Controller");
+        List<Quiz> quizzes = quizService.findAll();
+
+        quizzes.forEach(quiz -> {
+            Resource image = resourceLoader.getResource("classpath:/static/images/quiz/" + quiz.getImageUrl());
+            // 이미지가 없으면 none.png 반환
+            String imageUrl = image.exists() && image.isReadable() ? "/images/quiz/" + quiz.getImageUrl() : "/images/none.png";
+            quiz.setImageUrl(imageUrl);
+            quizService.saveQuiz(quiz);
+        });
+
+        List<QuizResponse> responses = quizzes.stream().map(quiz -> {
+            return QuizResponse.of(
+                    quiz.getId(),
+                    quiz.getUrl(),
+                    quiz.getImageUrl(),
+                    quiz.getAltText(),
+                    quiz.getTitle(),
+                    quiz.getDescription()
+            );
+        }).toList();
+        return ResponseEntity.ok(responses);
+    }*/
 
 
 }
